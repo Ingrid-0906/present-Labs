43a79f7b47d4819b23950057e4bf4721971b9091
Ingrid_Cadu <icardoso792@gmail.com>
2024-04-04 15:20:49 -0300

    Remove a função `extrair_dados_apple_e_google`.

--- calc.py
+++ calc.py
@@ -4,7 +4,3 @@ def extrair_dados_yahoo_finance(acao):
     dados = yf.download(acao, start="2021-01-01", end="2021-12-31")
     return dados
 
-def extrair_dados_apple_e_google():
-    dados_apple = extrair_dados_yahoo_finance("AAPL")
-    dados_google = extrair_dados_yahoo_finance("GOOGL")
-    return dados_apple, dados_google
